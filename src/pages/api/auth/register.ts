bfe42174cdbe24b0982a06083c70d7e853e61fde
Jared Muñoz <amunozbaez669@gmail.com>
2024-05-18 11:52:58 -0600

    refactor: Update user registration API endpoint

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -3,35 +3,44 @@ import { db, User, eq } from "astro:db";
 import * as bcrypt from "bcrypt";
 
 export const POST: APIRoute = async ({ request }) => {
+    const body = await request.json();
 
-    const body = await request.json()
-
-    const isSomeWithSameEmail = (await db.select().from(User).where(eq(User.email, body.email)))[0]
-    if (isSomeWithSameEmail) {
-        return new Response("El correo electrónico ya está en uso.", {
-            status: 400,
-            statusText: "El correo electrónico ya está en uso.",
-        })
+    try {
+        const isSomeWithSameEmail = (await db.select().from(User).where(eq(User.email, body.email)))[0];
+        if (isSomeWithSameEmail) {
+            return new Response("El correo electrónico ya está en uso.", {
+                status: 400,
+                statusText: "El correo electrónico ya está en uso.",
+            });
+        }
+    }
+    catch (error) {
+        console.error(error);
+        return new Response("Error al verificar el correo electrónico.", {
+            status: 500,
+            statusText: "Error al verificar el correo electrónico.",
+        });
     }
 
-    const id = crypto.randomUUID()
-    const password = await bcrypt.hash(body.password, 10)
+    const id = crypto.randomUUID();
+    const password = await bcrypt.hash(body.password, 10);
 
-    console.log({ id, body })
+    console.log({ id, body });
 
     try {
         await db.insert(User).values([{
             ...body,
             password,
             id,
-        }])
-    }
-    catch (error) {
-        console.error(error)
+        }]);
+    } catch (error) {
+        // Handle the error appropriately
+        console.error(error);
         return new Response("Error al registrar el usuario.", {
             status: 500,
             statusText: "Error al registrar el usuario.",
-        })
+        });
     }
-    return new Response('Usuario registrado correctamente', { status: 201 })
-}
+
+    return new Response('Usuario registrado correctamente', { status: 201 });
+};
