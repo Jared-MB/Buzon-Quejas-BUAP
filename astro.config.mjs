d460b228233627a22ea90a1e98c2476ceea8d70e
Jared Muñoz <amunozbaez669@gmail.com>
2024-05-17 20:20:09 -0600

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
 import react from "@astrojs/react";
-
 import auth from "auth-astro";
-
 import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,7 +13,5 @@ export default defineConfig({
     applyBaseStyles: false
   }), db(), react(), auth()],
   output: 'server',
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: vercel()
 });
